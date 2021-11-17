ac03444d53452bca46e5efeaaa4b7368735dfc1f
Jillian Anderson <59696481+jilland@users.noreply.github.com>
2021-11-17 10:55:52 -0500

    removed * from delete method to correct syntax error

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.patch("/app/update/user/:id", (req, res) => {
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
 app.delete("/app/delete/user/:id", (req, res) => {
 	id = req.params.id
-	const stmt = db.prepare("DELETE * FROM userinfo WHERE id = ?");
+	const stmt = db.prepare("DELETE FROM userinfo WHERE id = ?");
 	const info = stmt.run(id);
 	res.status(200).json({"message": "1 record deleted: ID "+ id + " (200)"});
 });
