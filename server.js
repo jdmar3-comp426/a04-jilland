d3c50430e6445f64e739ff69452cf9e22b9f2cca
Jillian Anderson <59696481+jilland@users.noreply.github.com>
2021-11-16 21:25:48 -0500

    First draft, all functions minus update and create as baseline test

--- server.js
+++ server.js
@@ -2,14 +2,17 @@
 var express = require("express")
 var app = express()
 // Require database SCRIPT file
+var db = require('./database.js')
 
 // Require md5 MODULE
+var md5 = require("md5")
 
 // Make Express use its own built-in body parser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set server port
+var HTTP_PORT = 5000
 
 // Start server
 app.listen(HTTP_PORT, () => {
@@ -22,8 +25,13 @@ app.get("/app/", (req, res, next) => {
 });
 
 // Define other CRUD API endpoints using express.js and better-sqlite3
-// CREATE a new user (HTTP method POST) at endpoint /app/new/
 
+// CREATE a new user (HTTP method POST) at endpoint /app/new/
+app.get("/app/new/user", (req, res, next) => {	
+	//info, run returns
+});
+
+// given in template
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
 app.get("/app/users", (req, res) => {	
 	const stmt = db.prepare("SELECT * FROM userinfo").all();
@@ -31,13 +39,26 @@ app.get("/app/users", (req, res) => {
 });
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
+app.get("/app/user/:id", (req, res) => {	
+	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = ?");
+	res.json(stmt);
+	res.status(200);
+});
 
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
+app.patch("/app/update/user/:id", (req, res) => {
+
+});
 
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
+app.delete("/app/delete/user/:id", (req, res) => {
+	const stmt = db.prepare("DELETE * FROM userinfo WHERE id = ?");
+	res.json(stmt);
+	res.status(200);
+});
 
 // Default response for any other request
 app.use(function(req, res){
-	res.json({"message":"Endpoint not found. (404)"});
+	res.json({"message":"Your API is working!"});
     res.status(404);
 });
